85c9724dafec98e944cc6b214d41af6249ff0bdb
ismoilovdevml <ismoilovdevarchlinux@gmail.com>
2022-11-14 12:09:44 +0500

    db.rs updated

--- src/db.rs
+++ src/db.rs
@@ -10,5 +10,18 @@ pub type Pool = r2d2::Poll<ConnectionManager<PgConnection>>;
 
 pub fn init_pool(db_url: String) -> Pool {
     let manager = ConnectionManager::<PgConnection>::new(db_url);
-    r2d2::Pool::new(manager).expect("db_url")
+    r2d2::Pool::new(manager).expect("db pool failure")
+}
+pub struct Conn(pub r2d2::PoolConnection<ConnectionManager<PgConnection>>);
+
+impl<'a, 'r> FromRequest<'a, 'r> for Conn{
+    type Error = ();
+
+    fn from_request(request: &'a Request<'r>) -> request::OutCome<Conn, ()>{
+        let pool = request.guard::<State<Pool>>()?;
+        match pool.get() {
+            Ok(connn) => OutCome::Success(Conn(conn)),
+            Err(_) => OutCome::Failure((Status::ServiceUnavailable, ())),
+        }
+    }
 }
