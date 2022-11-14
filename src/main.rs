d7948eb598d208f35f48383ac538de55fd294ea6
ismoilovdevml <ismoilovdevarchlinux@gmail.com>
2022-11-14 12:34:56 +0500

    updated

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ extern  crate r2d2;
 extern crate r2d2_diesel;
 
 #[macro_use]
-extern crate roxket;
+extern crate rocket;
 extern crate rocket_contrib;
 
 #[macro_use]
@@ -26,4 +26,35 @@ mod db;
 mod models;
 mod routes;
 mod schema;
+
+fn rocket() -> rocket::Rocket {
+    dotenv().ok();
+
+    
+
+
+    let pool = db::init_pool(database_url);
+    rocket::ignite()
+        .manage(pool)
+        .mount(
+            "/api/v1",
+            routes![get_all, new_user, find_user],
+        )
+}
+
+fn main() {
+    let _output = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .args(&["/C", "cd ui && npm start"])
+            .spawn()
+            .expect("UI ilovasini ishga tushirib bo'lmadi")
+    } else {
+        Command::new("sh")
+            .arg("-c")
+            .arg("cd ui && npm start")
+            .spawn()
+            .expect("UI ilovasini ishga tushirib bo'lmadi")
+    };
+    rocket().launch();
+}
 
